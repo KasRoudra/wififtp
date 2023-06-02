c7b1703ba4692874335d0006a3d1e386a0074a58
KasRoudra <kasroudrard@gmail.com>
2023-06-02 10:04:52 +0600

    Completely refractored wififtp

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # WiFi-FTP
-### [+] Created By <a href="https://github.com/KasRoudra">KasRoudra</a>
+
 ### [+] Description :
 ***WiFi-FTP is a tool to create a simple ftp server in local network. Anyone under same wifi/router can read/write/modify the folder you shared.***
 <h2 align="center">Share files under same Wi-Fi!</h2>
 
 ### [+] Installation
 
-```apt install figlet python3 python3-pip -y```
+```apt install python3 python3-pip -y```
 
 ```pip3 install pyftpdlib```
 
@@ -22,19 +22,35 @@
 ```
 git clone https://github.com/KasRoudra/wififtp && cd wififtp && python3 ftp.py
 ```
-### [+]Features 
-#### You can change both port and shared folder. Without change default port will be 8000 and default folder will be the folder from which the file is executed!
+### [*] Features
+ - You can customize both port and shared folder. Without change, default port will be 2121 and default folder will be the folder from which the file is executed!
+ - Now WiFi-FTP also support arguments 
 
-## [+]!Caution:
+### [-] Options
+```
+usage: ftp.py [-h] [-p PORT] [-d DIRECTORY]
 
-### You must need "Python" installed in your operating system. If you use windows you also have to enable python from "Windows Firewall". If you can't enable python from firewall, you may need to disable firewall while using ftp!
-### Your sharing link will be like "ftp://192.168.0.105:8080". Make sure you are connected to router/Wi-Fi to aceess folder from other device!
+options:
+  -h, --help            show this help message and exit
+  -p PORT, --port PORT  WiFi-FTP's server port [Default: 2121]
+  -d DIRECTORY, --directory DIRECTORY
+                        Directory where server will start [Default: /home/kasroudra/]
+```
+## [+] Caution:
+
+### You must need "Python" installed in your operating system. If you use firewall you also have to enable python from "Windows Firewall". If you can't enable python from firewall, you may need to disable firewall while using ftp!
+### Your sharing link will be like "ftp://192.168.0.105:2121". Make sure you are connected to same router/Wi-Fi to access folder from other device!
 ## [+] Screenshot:
-<img src="https://github.com/KasRoudra/wififtp/blob/main/ss.jpeg">
+<img src="https://github.com/KasRoudra/wififtp/blob/main/ss.jpg">
 
-## [+] Find Me on :
-<ul>
-<li><a href="https://facebook.com/KasRoudra"><img src="https://github.com/KasRoudra/kasweb/raw/main/assets/facebook.png" alt="facebook" width="20px" height="20px"></a></li>
-<li><a href="https://m.me/KasRoudra"><img src="https://github.com/KasRoudra/kasweb/raw/main/assets/messenger.png" alt="messenger" width="20px" height="20px"></a></li>
-<li><a href="mailto:kasroudrard@gmail.com"><img src="https://github.com/KasRoudra/kasweb/raw/main/assets/gmail.png" alt="email" width="20px" height="20px"></a></li>
-</ul>
+## [~] Find Me on :
+
+- [![Github](https://img.shields.io/badge/Github-KasRoudra-green?style=for-the-badge&logo=github)](https://github.com/KasRoudra)
+
+- [![Gmail](https://img.shields.io/badge/Gmail-KasRoudra-green?style=for-the-badge&logo=gmail)](mailto:kasroudrakrd@gmail.com)
+
+- [![Facebook](https://img.shields.io/badge/Facebook-KasRoudra-green?style=for-the-badge&logo=facebook)](https://facebook.com/KasRoudra)
+
+- [![Messenger](https://img.shields.io/badge/Messenger-KasRoudra-green?style=for-the-badge&logo=messenger)](https://m.me/KasRoudra)
+
+- [![Telegram](https://img.shields.io/badge/Telegram-KasRoudra-indigo?style=for-the-badge&logo=telegram)](https://t.me/KasRoudra)
--- ftp.py
+++ ftp.py
@@ -1,88 +1,254 @@
-import os,sys,socket
+from argparse import ArgumentParser
+from os import (
+    name,
+    getcwd,
+    getenv
+)
+from os.path import (
+    isfile,
+    isdir,
+    dirname
+)
+from socket import (
+    socket,
+    error,
+    AF_INET,
+    SOCK_DGRAM,
+    SOCK_STREAM
+)
+from subprocess import (
+    run,
+    DEVNULL,
+    PIPE,
+    Popen
+)
+from sys import (
+    executable,
+    stdout
+)
+from time import sleep
+from re import escape
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.settimeout(10)
-try:
-	s.connect(("8.8.8.8", 80))
-	ip=s.getsockname()[0]
-	if ((ip.find("192") != -1) | (ip.find("172") != -1)):
-	    pass
-	else:
-	    print("You are not using any local network!\nPlease connect to a hotspot or router/Wi-Fi!")
-	    exit()
-except socket.error:
-    print("No internet")
-    exit()
-s.close()
-portno=input("Enter the port no:(Press enter to keep default 8000)\n")
 
-if (portno==""):
-    print("Port supported!")
-else:
-    try:
-        portno=int(portno)
-        if (portno>1023 & portno<65536):
-            print("Port supported")
+# Color snippets
+black = "\033[0;30m"
+red = "\033[0;31m"
+bred = "\033[1;31m"
+green = "\033[0;32m"
+bgreen = "\033[1;32m"
+yellow = "\033[0;33m"
+byellow = "\033[1;33m"
+blue = "\033[0;34m"
+bblue = "\033[1;34m"
+purple = "\033[0;35m"
+bpurple = "\033[1;35m"
+cyan = "\033[0;36m"
+bcyan = "\033[1;36m"
+white = "\033[0;37m"
+nc = "\033[00m"
+
+version = "1.0"
+
+# Regular Snippets
+ask = f"{green}[{white}?{green}] {yellow}"
+success = f"{yellow}[{white}√{yellow}] {green}"
+error = f"{blue}[{white}!{blue}] {red}"
+info = f"{yellow}[{white}+{yellow}] {cyan}"
+info2 = f"{green}[{white}•{green}] {purple}"
+
+default_dir = getcwd()
+default_port = 2121
+
+banner = f"""
+{red}__        ___ _____ _       _____ _____ ____  
+{blue}\ \      / (_)  ___(_)     |  ___|_   _|  _ \  
+{green} \ \ /\ / /| | |_  | |_____| |_    | | | |_) |
+{cyan}  \ V  V / | |  _| | |_____|  _|   | | |  __/ 
+{purple}   \_/\_/  |_|_|   |_|     |_|     |_| |_| 
+{blue}                                    [{green}v{cyan}{version}{blue}]   
+{cyan}                            [{blue}By {green}KasRoudra{cyan}]{nc}
+"""
+
+argparser = ArgumentParser()
+
+argparser.add_argument("-p", "--port", type=int, help=f"WiFi-FTP's server port [Default: {default_port}]")
+argparser.add_argument("-d", "--directory", help=f"Directory where server will start [Default: {default_dir}]")
+
+args = argparser.parse_args()
+
+arg_port = args.port
+arg_directory = args.directory
+
+
+# Check if a package is installed
+def is_installed(package):
+    return shell(f"command -v {package}", True).returncode == 0
+
+# Print lines slowly
+def sprint(text, second=0.05):
+    for line in text + '\n':
+        stdout.write(line)
+        stdout.flush()
+        sleep(second)
+
+# Prints colorful texts
+def lolcat(text, slow=True, second=0.05):
+    if is_installed("lolcati"):
+        run(["lolcat"], input=text, text=True)
+    else:
+        if slow:
+            sprint(text, second)
         else:
-            print("Port not allowed!Range 1024-65535")
+            print(text)
+
+
+def show_banner():
+    shell(["clear", "cls"][name == "nt"])
+    lolcat(banner, second=0.01)
+
+
+# Run shell commands in python
+def shell(command, capture_output=False):
+    return run(command, shell=True, capture_output=capture_output)
+
+# Install pip requirements if not found
+def inst_deps():
+    retry = 3
+    shell("stty -echoctl")
+    while retry:
+        try:
+            import pyftpdlib
+            break
+        except ImportError:
+            shell(f"{executable} -m pip install pyftpdlib")
+        except Exception as e:
+            print(f"{error}{str(e)}")
+        if retry == 1:
+            print(f"{error}Install pyftpdlib manually!{nc}")
+            exit(1)
+        retry -= 1
+
+# Swap bettween ~ and /home/user
+def pretty_path(path, rel=True):
+    new_path = path
+    home = getenv("HOME")
+    if path.startswith("\\"):
+        new_path = path[1:]
+    if "~" in path:
+        new_path = path.replace("~", home)
+    if home in path and rel:
+        new_path = path.replace(home, "~")
+    return new_path
+
+# Check if a port is being used
+def is_available_port(port):
+    with socket(AF_INET, SOCK_STREAM) as connection:
+        return connection.connect_ex(("localhost", int(port))) != 0
+
+# Get user ip address
+def get_ip():
+    with socket(AF_INET, SOCK_DGRAM) as connection:
+        connection.settimeout(10)
+        try:
+            connection.connect(("8.8.8.8", 80))
+            return connection.getsockname()[0]
+        except error:
+            print(f"{error}No internet")
             exit()
-    except:
-        print("Port must be a number!")
+        except Exception as e:
+            print(f"{error}{str(e)}")
+            exit()
+
+
+# Check if local ip
+def check_local():
+    ip = get_ip()
+    if not ip.startswith("192") and not ip.startswith("172"):
+        print(
+            f"{error}You are not using any local network!\nPlease connect to a hotspot or router/Wi-Fi!{nc}"
+        )
         exit()
-path=input("Enter the path:(Press enter to keep default current path)\n")
-if (os.path.exists(path) | (path=="")):
-    print("Path supported!")
-else:
-    print("Path error!")
-    exit()
-    
-alltext='''from pyftpdlib.authorizers import DummyAuthorizer
-from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer
 
-authorizer = DummyAuthorizer()
-authorizer.add_user("user", "12345","/sdcard", perm="elradfmw")
-authorizer.add_anonymous("/sdcard", perm="elradfmw")
+# Use current directory as ftp path if no path is specified by user
+def get_path():
+    while True:
+        if arg_directory is None:
+            path = pretty_path(
+                escape(
+                    input(
+                        f"{ask}Enter path (Default: {green}{default_dir}{yellow})\n-> "
+                    )
+                ),
+                rel=False
+            )
+        else:
+            path = arg_directory
+        if isfile(path):
+            path = dirname(path)
+            break
+        elif isdir(path):
+            break
+        elif path == "":
+            path = default_dir
+            break
+        else:
+            print(f"{error}Invalid path: {path}!{nc}")
+    print(f"{info}Choosing directory {pretty_path(path)}{nc}")
+    return path
 
-handler = FTPHandler
-handler.authorizer = authorizer
+# Use 2121 as ftp port if no port is specified by user
+def get_port():
+    while True:
+        if arg_port is None:
+            port = input(
+            f"{ask}Enter port (Default: {green}{default_port}{yellow})\n-> ")
+        else:
+            port = str(arg_port)
+        if port.isdigit() and int(port) > 1023 and int(port) < 65536 and is_available_port(port):
+            break
+        elif port == "" and is_available_port(default_port):
+            port = default_port
+            break
+        else:
+            print(f"{error}Invalid port: {port}!{nc}")
+    print(f"{info}Choosing port {port}{nc}")
+    return port
 
-try:
-	server = FTPServer(("0.0.0.0", 8000), handler)
-	server.serve_forever()
-except:
-	print("Error! Change port or restart terminal!")'''
+# https://github.com/giampaolo/pyftpdlib#quick-start
+def ftp(path, port):
+    from pyftpdlib.authorizers import DummyAuthorizer
+    from pyftpdlib.handlers import FTPHandler
+    from pyftpdlib.servers import FTPServer
+    authorizer = DummyAuthorizer()
+    authorizer.add_user("user", "12345", path, perm="elradfmw")
+    authorizer.add_anonymous(path, perm="elradfmw")
 
-def replacer(alltext,portno, path):
-    portno=str(portno)
-    alltext1=alltext.replace("8000", portno)
-    alltext2=alltext1.replace("/sdcard", path, 2)
-    os.system("rm -rf .ftptemp")
-    opr=open(".ftptemp", 'a')
-    opr.write(alltext2)
-    opr.close()
-    os.system("clear")
-    os.system("figlet WiFi-FTP")
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(10)
-    s.connect(("8.8.8.8", 80))
-    ip=s.getsockname()[0]
-    print("Your FTP link is:\nftp://"+ip+":"+portno)
-    os.system("python3 .ftptemp")
-    
-while True:
-    if ((portno=="") & (path=="")):
-        portno="8000"
-        path=os.getcwd()
-        replacer(alltext,portno,path)
-        break
-    if ((portno!="") & (path=="")):
-        path=os.getcwd()
-        replacer(alltext,portno,path)
-        break
-    if ((portno=="") & (path!="")):
-        portno="8000"
-        replacer(alltext,portno,path)
-        break
-    if ((portno!="") & (path!="")):
-        replacer(alltext,portno,path)
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    server = FTPServer(("0.0.0.0", port), handler)
+    server.serve_forever()
+
+# Print ftp address and start ftp by taking inputs
+def start_ftp():
+    ip = get_ip()
+    path = get_path()
+    port = get_port()
+    lolcat(f"{info2}Your FTP link is: {green}ftp://{ip}:{port}{nc}")
+    ftp(path, port)
+
+# StartPoint of script
+def main():
+    if __name__ == "__main__":
+        try:
+            inst_deps()
+            show_banner()
+            check_local()
+            start_ftp()
+        except KeyboardInterrupt:
+            print(f"\n{info}Closing script....{nc}")
+        except Exception as e:
+            print(f"{error}{str(e)}")
+
+
+main()
